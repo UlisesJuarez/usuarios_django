793f6ade5ae7c6afe31725c51599e75fd0d7d35f
UlisesJuarez <uje170399@gmail.com>
2023-06-20 20:24:43 -0600

    creacion de super usuario con nuevo model user

--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -18,8 +18,11 @@ class User(AbstractBaseUser,PermissionsMixin):
     nombres = models.CharField(max_length=30,blank=True)
     apellidos = models.CharField(max_length=30,blank=True)
     genero = models.CharField(max_length=1,choices=GENDER_CHOICES,blank=True)
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD='username'
+
+    REQUIRED_FIELDS=['email',]
 
     objects= UserManager()
 
